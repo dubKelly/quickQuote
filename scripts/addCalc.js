a66c22684aca9c449a288b3ddf4104b6a6b443c1
dubKelly <js.neeb1780@gmail.com>
2016-10-03 19:16:48 -0400

    not += properly; only logging latest array value

--- scripts/addCalc.js
+++ scripts/addCalc.js
@@ -2,7 +2,7 @@
 // dormers
 
 document.getElementById("add").onclick = function addDormer() {
-	var container = document.getElementById("container");
+	var container = document.getElementById('container');
 	var dimAdorm = document.createElement("input");
 	var dimBdorm = document.createElement("input");
 	var dorm_pitch = document.createElement("input");
@@ -15,7 +15,7 @@ document.getElementById("add").onclick = function addDormer() {
 	dimBdorm.value = 4;
 	dorm_pitch.className = "dorm_pitch";
 	dorm_pitch.placeholder = "Pitch";
-	dorm_pitch.value = 24;
+	dorm_pitch.value = 4;
 	dimCdorm.className = "dimCdorm";
 	dimCdorm.placeholder = "Dimension C";
 	dimCdorm.value = 2;
@@ -47,12 +47,12 @@ document.getElementById("calc").onclick = function calc() {
 
 	// dormers
 
-		var mainDorm_runArr = document.getElementsByClassName("dimAdorm"); // / 2
+		var mainDorm_runArr = document.getElementsByClassName('dimAdorm'); // / 2
 	
 	for (var i = 0; i < mainDorm_runArr.length; i++) {
 
 		var mainDorm_run = (+mainDorm_runArr[i].value / 2);
-		var mainDorm_slopeArr = document.getElementsByClassName("dorm_pitch"); // / 12
+		var mainDorm_slopeArr = document.getElementsByClassName('dorm_pitch'); // / 12
 		var mainDorm_slope = (+mainDorm_slopeArr[i].value / 12);
 /*		var mainDorm_rise = mainDorm_run * mainDorm_slope;
 		var mainDorm_hyp = Math.sqrt(Math.pow(mainDorm_rise, 2) + Math.pow(mainDorm_run, 2));
@@ -72,21 +72,19 @@ document.getElementById("calc").onclick = function calc() {
 //		var mainDormOvr_surA = (mainDormOvr_peak * mainDormOvr_hyp) * 2;
 
 		var mainDorm_totalSurA = 0;
-		var test2 = 0;
 //		var mainDorm_totalCap = 10;
 			
 //		mainDorm_totalSurA += +mainDorm_surA[i].value + +mainDormOvr_surA[i].value - +mainDormCvr_surA[i].value;
 //		mainDorm_totalCap += +mainDorm_peak[i].value + +mainDormOvr_peak[i].value;
 
-		mainDorm_totalSurA = (Math.sqrt(Math.pow((+mainDorm_runArr[i].value * +mainDorm_slopeArr[i].value), 2) + Math.pow(+mainDorm_runArr[i].value, 2)));
-		test2 += mainDorm_totalSurA;
+		mainDorm_totalSurA += +mainDorm_runArr[i].value * +mainDorm_slopeArr[i].value;
 	}
 //	var totalSurA = (main_totalSurA + mainDorm_totalSurA) / 33.3;
 //	var totalCap = (main_totalCap + mainDorm_totalCap) / 29.5;
 //	var totalValley = mainDorm_valley / 8;
 
-	// *test* 
-	document.getElementById("total").innerHTML = test2;
+	// *test* document.getElementById("total").innerHTML = 
+	console.log(mainDorm_totalSurA);
 /*	document.getElementById("total").innerHTML = 
 	"Total SurA: " 
 	+ main_totalSurA.toFixed(2)
