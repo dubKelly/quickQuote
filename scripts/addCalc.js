b801dc30674857fe14fa34588c35129eb714f1ec
dubKelly <js.neeb1780@gmail.com>
2016-10-13 02:34:47 -0400

    added mainDormPie

--- scripts/addCalc.js
+++ scripts/addCalc.js
@@ -1,70 +1,88 @@
 var dynId = 0;
 var negId = 0;
-	function idGen() {
-		dynId++;
-		negId = dynId - 1;
-		console.log(dynId);
-	}
-var mainInputArr = [];
+function idGen() {
+	dynId++;
+	negId = dynId - 1;
+	console.log(dynId);
+}
+var addArr = [];
 function mainPush() {
-	mainInputArr.push("mainAdd");
-	console.log(mainInputArr);
+	addArr.push("mainAdd");
+	console.log(addArr);
 }
 function piePush() {
-	mainInputArr.push("pieAdd");
-	console.log(mainInputArr);
+	addArr.push("pieAdd");
+	console.log(addArr);
 }
-function mainAdd() {
+
+function addDorm() {
+	document.getElementById("mainList").style.display = "none";
+	document.getElementById("subInput" + negId).style.display = "none";
+	document.getElementById("dormInput").style.display = "block";
+}
+function add() {
 	document.getElementById("mainInput").style.display = "none";
 	document.getElementById("mainList").style.display = "block";
-
-
-	document.getElementById("gableDorm").onclick = function addDormer() {
-		document.getElementById("mainList").style.display = "none";
-		document.getElementById("subInput" + negId).style.display = "none";
-		document.getElementById("dormInput").style.display = "block";
-		var dormInput = document.getElementById("dormInput");
-		var subInput = document.createElement("div");
-		subInput.className = "inner";
-		subInput.id = "subInput" + dynId;
-		dormInput.appendChild(subInput);
-		var dimAdorm = document.createElement("input");
-		dimAdorm.className = "dimAdorm";
-		dimAdorm.placeholder = "Dimension A";
-		dimAdorm.value = 10;
-		subInput.appendChild(dimAdorm);
-		var dimBdorm = document.createElement("input");
-		dimBdorm.className = "dimBdorm";
-		dimBdorm.placeholder = "Dimension B";
-		dimBdorm.value = 10;
-		subInput.appendChild(dimBdorm);
-		var dorm_pitch = document.createElement("input");
-		dorm_pitch.className = "dorm_pitch";
-		dorm_pitch.placeholder = "Pitch";
-		dorm_pitch.value = 10;
-		subInput.appendChild(dorm_pitch);
-		var dimCdorm = document.createElement("input");
-		dimCdorm.className = "dimCdorm";
-		dimCdorm.placeholder = "Dimension C";
-		dimCdorm.value = 6;
-		subInput.appendChild(dimCdorm);
-		var done = document.createElement("button");
-		done.className = "done";
-		done.innerHTML = "Done";
-		subInput.appendChild(done);
-		var doneArr = document.getElementsByClassName("done");
-		for (var i = 0; i < doneArr.length; i++) {
-			doneArr[i].onclick = function done() {
-			document.getElementById("dormInput").style.display = "none";
-			document.getElementById("mainInput").style.display = "block";
-			}
+	var dormInput = document.getElementById("dormInput");
+	var subInput = document.createElement("div");
+	subInput.className = "inner";
+	subInput.id = "subInput" + dynId;
+	dormInput.appendChild(subInput);
+	var dimAdorm = document.createElement("input");
+	dimAdorm.className = "dimAdorm";
+	dimAdorm.placeholder = "Dimension A";
+	dimAdorm.value = 10;
+	subInput.appendChild(dimAdorm);
+	var dimBdorm = document.createElement("input");
+	dimBdorm.className = "dimBdorm";
+	dimBdorm.placeholder = "Dimension B";
+	dimBdorm.value = 10;
+	subInput.appendChild(dimBdorm);
+	var dorm_pitch = document.createElement("input");
+	dorm_pitch.className = "dorm_pitch";
+	dorm_pitch.placeholder = "Pitch";
+	dorm_pitch.value = 10;
+	subInput.appendChild(dorm_pitch);
+	var dimCdorm = document.createElement("input");
+	dimCdorm.className = "dimCdorm";
+	dimCdorm.placeholder = "Dimension C";
+	dimCdorm.value = 6;
+	subInput.appendChild(dimCdorm);
+	var dormPie_pitch = document.createElement("input");
+	dormPie_pitch.className = "dormPie_pitch";
+	dormPie_pitch.id = "dormPie_pitch" + dynId;
+	dormPie_pitch.placeholder = "Pie Pitch";
+	subInput.appendChild(dormPie_pitch);
+	var done = document.createElement("button");
+	done.className = "done";
+	done.innerHTML = "Done";
+	subInput.appendChild(done);
+	var doneArr = document.getElementsByClassName("done");
+	for (var i = 0; i < doneArr.length; i++) {
+		doneArr[i].onclick = function done() {
+		document.getElementById("dormInput").style.display = "none";
+		document.getElementById("mainInput").style.display = "block";
 		}
-		var dormAdd = document.createElement("button");
-		dormAdd.id = "dormAdd";
-		dormAdd.innerHTML = "Add+";
-		subInput.appendChild(dormAdd);
 	}
 }
+function dormDisplay() {
+	if (addArr[addArr.length - 1] === "mainAdd") {
+		document.getElementById("dormPie_pitch" + dynId).style.display = "none";
+	}
+	else if (addArr[addArr.length - 1] === "pieAdd") {
+		console.log(addArr[addArr.length-1]);
+		document.getElementById("dormPie_pitch" + dynId).style.display = "block";
+	}
+}
+document.getElementById("mainAdd").addEventListener("click", idGen);
+document.getElementById("mainAdd").addEventListener("click", add);
+document.getElementById("mainAdd").addEventListener("click", mainPush);
+document.getElementById("mainPieAdd").addEventListener("click", idGen);
+document.getElementById("mainPieAdd").addEventListener("click", add);
+document.getElementById("mainPieAdd").addEventListener("click", piePush);
+document.getElementById("gableDorm").addEventListener("click", addDorm);
+document.getElementById("gableDorm").addEventListener("click", dormDisplay);
+
 
 document.getElementById("calc").onclick = function calc() {
 
@@ -79,8 +97,8 @@ document.getElementById("calc").onclick = function calc() {
 	var mainPie_slope = (+document.getElementById("mainPie_pitch").value) / 12;
 	var mainPie_peak = main_rise / mainPie_slope;
 	var mainPie_hyp = Math.sqrt(Math.pow(main_rise, 2) + Math.pow(mainPie_peak, 2));
-	var mainPie_surA = (main_run * mainPie_hyp) * 2;
 	var mainPie_hip = (Math.sqrt(Math.pow((main_run / 2), 2) + Math.pow(mainPie_hyp, 2))) * 4;
+	var mainPie_surA = (main_run * mainPie_hyp) * 2;
 	var mainPieCvr_surA = (mainPie_peak * main_hyp) * 2;
 
 	var main_totalSurA = (main_surA + mainPie_surA - mainPieCvr_surA);
@@ -89,35 +107,40 @@ document.getElementById("calc").onclick = function calc() {
 	// mainDormers
 
 		var mainDorm_runArr = document.getElementsByClassName("dimAdorm");
-		var mainDorm_slopeArr = document.getElementsByClassName("dorm_pitch");
+		var mainDorm_pitchArr = document.getElementsByClassName("dorm_pitch");
 		var mainDormOvr_runArr = document.getElementsByClassName("dimBdorm");
 		var mainDormOvr_peakArr = document.getElementsByClassName("dimCdorm"); 
+		var mainDormPie_pitchArr = document.getElementsByClassName("dormPie_pitch");
 		var mainDorm_totalSurA = 0;
 		var mainDorm_totalCap = 0;
 		var mainDorm_totalValley = 0;
 	
 	for (var i = 0; i < mainDorm_runArr.length; i++) {
 
-		var mainDorm_run = (+mainDorm_runArr[i].value / 2);
-		var mainDorm_slope = (+mainDorm_slopeArr[i].value / 12);
+		var mainDorm_run = +mainDorm_runArr[i].value / 2;
+		var mainDorm_slope = +mainDorm_pitchArr[i].value / 12;
 		var mainDorm_rise = mainDorm_run * mainDorm_slope;
 		var mainDorm_hyp = Math.sqrt(Math.pow(mainDorm_rise, 2) + Math.pow(mainDorm_run, 2));
-		var mainDorm_peak = mainDorm_rise / main_slope;
+		if (addArr[addArr.length - 1] === "mainAdd") {
+			var mainDorm_peak = mainDorm_rise / main_slope;
+		}
+		else if (addArr[addArr.length - 1] === "pieAdd") {
+			mainDorm_peak = mainDorm_rise / mainPie_slope;
+		}
 		var mainDorm_valley = (Math.sqrt(Math.pow(mainDorm_peak, 2) + Math.pow(mainDorm_hyp, 2))) * 2;
 		var mainDorm_surA = mainDorm_peak * mainDorm_hyp;
 
 		var mainDormCvr_hyp = Math.sqrt(Math.pow(mainDorm_rise, 2) + Math.pow(mainDorm_peak));
 		var mainDormCvr_surA = mainDorm_run * mainDorm_hyp;
 
-		var mainDormOvr_run = (+mainDormOvr_runArr[i].value / 2);
+		var mainDormOvr_run = +mainDormOvr_runArr[i].value / 2;
 		var mainDormOvr_rise = mainDormOvr_run * mainDorm_slope;
 		var mainDormOvr_hyp = Math.sqrt(Math.pow(mainDormOvr_rise, 2) + Math.pow(mainDormOvr_run, 2));
-		var mainDormOvr_peak = (+mainDormOvr_peakArr[i].value || 0);
+		var mainDormOvr_peak = +mainDormOvr_peakArr[i].value || 0;
 		var mainDormOvr_surA = (mainDormOvr_peak * mainDormOvr_hyp) * 2;
 
 		var mainDormOvrH_rise = mainDormOvr_peak * main_slope;
 		var mainDormOvrH_hyp = Math.sqrt(Math.pow(mainDormOvrH_rise, 2) + Math.pow(mainDormOvr_peak, 2))
-
 		if (mainDormOvr_run > mainDorm_run || mainDormOvr_peak === 0) {
 			var mainDormOvrH_surA = 0;
 		}
@@ -125,25 +148,44 @@ document.getElementById("calc").onclick = function calc() {
 			mainDormOvrH_surA = (mainDormOvrH_hyp + mainDorm_run) * 2;
 		}
 
-		mainDorm_totalCap += mainDorm_peak + mainDormOvr_peak;
-		mainDorm_totalSurA += mainDorm_surA + mainDormOvr_surA + mainDormOvrH_surA - mainDormCvr_surA;
-		mainDorm_totalValley += mainDorm_valley;
+		var mainDormPie_slope = (+mainDormPie_pitchArr[i].value / 12) || 0;
+		var mainDormPie_peak = mainDorm_rise / mainDormPie_slope;
+		var mainDormPie_hyp = Math.sqrt(Math.pow(mainDorm_rise, 2) + Math.pow(mainDormPie_peak, 2));
+		var mainDormPie_hip = Math.sqrt(Math.pow(mainDormPie_hyp, 2) + Math.pow(mainDorm_run, 2));
+		var mainDormPie_surA = mainDorm_run * mainDormPie_hyp;
+		var mainDormPieCvr_surA = mainDormPie_peak * mainDorm_hyp;
+
+		mainDorm_totalCap += 
+		mainDorm_peak 
+		+ mainDormOvr_peak 
+		- mainDormPie_peak 
+		+ mainDormPie_hip;
+		mainDorm_totalSurA += 
+		mainDorm_surA 
+		+ mainDormOvr_surA 
+		+ mainDormOvrH_surA 
+		- mainDormCvr_surA 
+		+ mainDormPie_surA 
+		- mainDormPieCvr_surA;
+		mainDorm_totalValley += 
+		mainDorm_valley;
 	}
 	var totalSurA = (main_totalSurA + mainDorm_totalSurA) / 33.3;
 	var totalCap = (main_totalCap + mainDorm_totalCap) / 29.5;
 	var totalValley = mainDorm_totalValley / 8;
 
-	// *test* console.log(mainDormOvr_peak);
+	// *test* 
+	console.log(mainDorm_run);
 
 	document.getElementById("total").innerHTML = 
 	"Total SurA: " 
-	+ totalSurA.toFixed(2)
+	+ totalSurA.toFixed(1)
 	+ " Bundles" 
 	+ "<br>Total Cap: " 
-	+ totalCap.toFixed(2)
+	+ totalCap.toFixed(1)
 	+ " Bundles"
 	+ "<br>Total Valley: "
-	+ totalValley.toFixed(2)
+	+ totalValley.toFixed(1)
 	+ " Pieces";
 
 }
